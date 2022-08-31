7474b5267d2df8b7fcd835e72e49d25f4a6c291f
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-08-31 15:12:41 +0300

    Routes & admin check

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Admin\MessageController as AdminMessageController;
+use App\Http\Controllers\Admin\AdminUserController as AdminUserController;
 
 
 /*
@@ -25,24 +26,70 @@ Route::get('/', function () {
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/anasayfa', [HomeController::class, 'index'])->name('home');
 
+//**********************LOGİN LOGOUT PANEL ROUTES****************************
+Route::view('/loginuser', 'home.login')->name('loginuser');
+Route::view('/registeruser', 'home.register')->name('registeruser');
+Route::get('/logoutuser', [HomeController::class, 'logout'])->name('logoutuser');
+
+Route::view('/loginadmin', 'admin.login')->name('loginadmin');
+Route::post('/loginadmincheck', [HomeController::class, 'loginadmincheck'])->name('loginadmincheck');
+
+Route::get('/admin/login', [HomeController::class, 'login'])->name('admin_login');
+Route::post('/admin/logincheck', [HomeController::class, 'logincheck'])->name('admin_logincheck');
+Route::get('/admin/logout', [HomeController::class, 'logout'])->name('admin_logout');
+
+
 Route::get('/hakkimizda', [HomeController::class, 'about'])->name('about');
 Route::get('/forum', [HomeController::class, 'blog'])->name('blog');
 Route::get('/iletisim', [HomeController::class, 'contact'])->name('contact');
 Route::post('/storemessage', [HomeController::class, 'storemessage'])->name('storemessage');
 
-Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/', [AdminHomeController::class, 'index'])->name('index');
-    //Setting
-    Route::get('/setting', [AdminHomeController::class, 'setting'])->name('setting');
-    Route::post('/setting', [AdminHomeController::class, 'settingupdate'])->name('settingupdate');
-    //ADMIN MESSAGE ROUTES
-    Route::prefix('/messages')->name('message.')->controller(AdminMessageController::class)->group(function () {
-        Route::get('/', [AdminMessageController::class, 'index'])->name('index');
-        Route::get('/show/{id}', 'show')->name('show');
-        Route::post('/update/{id}', 'update')->name('update');
-        Route::get('/destroy/{id}', 'destroy')->name('destroy');
+Route::middleware('auth')->group(function () {
+    //USER ROUTES
+    Route::prefix('userpanel')->name('userpanel.')->controller(App\Http\Controllers\UserController::class)->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/reviews', 'reviews')->name('reviews');
+        Route::get('/reviewdestroy/{id}', 'reviewdestroy')->name('reviewdestroy');
+        Route::get('/orders', 'orders')->name('orders');
+        Route::get('/orderdetail/{id}', 'orderdetail')->name('orderdetail');
+        Route::get('/deleteproduct/{id}','deleteproduct')->name('deleteproduct');
+        Route::get('/favori-ürünlerim', 'favoriteproduct')->name('favoriteproduct');
     });
-}); //admin panel routes
+    //ADMİN PANEL ROUTES
+    Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
+        Route::get('/', [AdminHomeController::class, 'index'])->name('index');
+        //Setting
+        Route::get('/setting', [AdminHomeController::class, 'setting'])->name('setting');
+        Route::post('/setting', [AdminHomeController::class, 'settingupdate'])->name('settingupdate');
+        //ADMIN MESSAGE ROUTES
+        Route::prefix('/messages')->name('message.')->controller(AdminMessageController::class)->group(function () {
+            Route::get('/', [AdminMessageController::class, 'index'])->name('index');
+            Route::get('/show/{id}', 'show')->name('show');
+            Route::post('/update/{id}', 'update')->name('update');
+            Route::get('/destroy/{id}', 'destroy')->name('destroy');
+        });
+        //ADMIN FAQ ROUTES
+        Route::prefix('/faq')->name('faq.')->controller(AdminFaqController::class)->group(function () {
+            Route::get('/', [AdminFaqController::class, 'index'])->name('index');
+            Route::get('/create', 'create')->name('create');
+            Route::post('/store', 'store')->name('store');
+            Route::get('/edit/{id}', 'edit')->name('edit');
+            Route::post('/update/{id}', 'update')->name('update');
+            Route::get('/destroy/{id}', 'destroy')->name('destroy');
+            Route::get('/show/{id}', 'show')->name('show');
+        });
+        //ADMIN USER ROUTES
+        Route::prefix('/user')->name('user.')->controller(AdminUserController::class)->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/edit/{id}', 'edit')->name('edit');
+            Route::get('/show/{id}', 'show')->name('show');
+            Route::post('/update/{id}', 'update')->name('update');
+            Route::get('/destroy/{id}', 'destroy')->name('destroy');
+            Route::post('/addrole/{id}', 'addrole')->name('addrole');
+            Route::get('/destroyrole/{uid}/{rid}', 'destroyrole')->name('destroyrole');
+        });
+    }); //admin panel routes
+}); //user auth group
 
 Route::middleware([
     'auth:sanctum',

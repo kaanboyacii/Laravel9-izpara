968f479d655a0373fd6e8bbe75fe25fc5afedf01
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-08-31 13:26:44 +0300

    Setting in appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('*', function ($view) {
+            $setting = Setting::first();
+            $view->with('setting',  $setting);
+        });
     }
 }

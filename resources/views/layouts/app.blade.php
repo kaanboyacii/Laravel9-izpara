6e54b145d4e7fad8f63a5114eb46d4edccce9cb7
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-09-13 11:19:52 +0300

    App Blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,8 @@
 
         
         <!-- Scripts -->
-        <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
-
-
+        @vite('resources/css/app.css')
+        @vite('resources/js/app.js')
 
         <!-- Styles -->
         @livewireStyles
